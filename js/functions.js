99c412786d93782d76b70ea63248632ac77729da
NESPRAVEDLIVO <sergeysg194@gmail.com>
2024-12-07 21:17:21 +0500

    изменения 2.29

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
-var checkLength = function(string) {
-  var normalizerString =  string.replaceAll(' ', '').toLowerCase();
-  var voidString = "";
+let checkLength = function(string) {
+  let normalizerString =  string.replaceAll(' ', '').toLowerCase();
+  let voidString = "";
   for (let i = normalizerString.length - 1; i >= 0; i--) {
     voidString += normalizerString[i];
-  };
+  }
   return normalizerString === voidString;
-};
+}
