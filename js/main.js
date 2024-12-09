5b012c517bfb1877b7debeefc63f917924b0c23c
NESPRAVEDLIVO <sergeysg194@gmail.com>
2024-12-09 11:39:48 +0500

    задание 9.13

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import { generatePhotos } from './data.js';
 import { renderThumbnails } from './render-thumbnails.js';
+import { generatePhotos } from './data.js';
+import './form.js';
 
 // Генерация данных
 const photoArray = generatePhotos();
